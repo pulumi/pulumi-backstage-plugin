051643d8855f6339fd070b233b51c7ffbac1ea80
Engin Diri <engin.diri@ediri.de>
2024-10-02 15:14:29 +0200

    chore: update Backstage to 1.31.2

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -26,16 +26,31 @@ backend.add(import('@pulumi/plugin-catalog-backend-module-pulumi/alpha'));
 backend.add(
     import('@backstage/plugin-catalog-backend-module-scaffolder-entity-model'),
 );
+
+backend.add(import('@backstage/plugin-catalog-backend-module-logs'));
+
 // permission plugin
 backend.add(import('@backstage/plugin-permission-backend/alpha'));
+// See https://backstage.io/docs/permissions/getting-started for how to create your own permission policy
 backend.add(
     import('@backstage/plugin-permission-backend-module-allow-all-policy'),
 );
+
 // search plugin
 backend.add(import('@backstage/plugin-search-backend/alpha'));
+
+// search engine
+// See https://backstage.io/docs/features/search/search-engines
+backend.add(import('@backstage/plugin-search-backend-module-pg/alpha'));
+
+// search collators
 backend.add(import('@backstage/plugin-search-backend-module-catalog/alpha'));
 backend.add(import('@backstage/plugin-search-backend-module-techdocs/alpha'));
 
+// kubernetes
+backend.add(import('@backstage/plugin-kubernetes-backend/alpha'));
+
+// Pulumi
 backend.add(import('@pulumi/backstage-scaffolder-backend-pulumi'));
 backend.add(import('@backstage/plugin-scaffolder-backend-module-github'));
 
