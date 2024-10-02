2ebfd773edf7cdf8be2a4990fc130021add72197
Engin Diri <info@ediri.de>
2024-10-02 16:17:46 +0200

    feat: update publish to yarn npm publish (#36)

--- hack/publish.sh
+++ hack/publish.sh
@@ -17,4 +17,4 @@ yarn tsc
 yarn build
 
 # don't git commit or push
-yarn publish --new-version $STRIPPED_NEW_VERSION --no-git-tag-version
+yarn npm publish --tag $STRIPPED_NEW_VERSION --access public #--new-version $STRIPPED_NEW_VERSION --no-git-tag-version
