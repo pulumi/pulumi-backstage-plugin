fbd22b9aaa30c25a9419adbaa3bd72c6611893be
Engin Diri <info@ediri.de>
2025-05-24 18:03:22 +0200

    chore: update Backstage to 1.39.0 (#52)
    Signed-off-by: Engin Diri <engin.diri@ediri.de>

--- plugins/backstage-plugin-pulumi/src/components/PulumiIcon/PulumiIcon.tsx
+++ plugins/backstage-plugin-pulumi/src/components/PulumiIcon/PulumiIcon.tsx
@@ -1,7 +1,5 @@
 import { SvgIcon, SvgIconProps } from '@material-ui/core';
 
-import React from 'react';
-
 export const PulumiIcon = (props: SvgIconProps) => (
   <SvgIcon {...props} height="300" viewBox="0 0 300 300" fill="none" xmlns="http://www.w3.org/2000/svg">
   <circle cx="150" cy="150" r="150" fill="white"/>
